6c9be6574c0ac9c9c2dd570d08d50e025d326798
PokaChip5625 <0108523.astrtry@gmail.com>
2026-05-05 23:37:47 +0900

    함수 계산기
    함수란 거창한 것이 아닌 전에 했던 내 계산기를 나눠서 간소화 하는 것이다.
    우선 오류가 났을때 반복해서 경고문을 보내주는 명령어를 만들고 그것을 한 변수의 특성으로 덮어서 만들어 준다. (코드에서 난 int inputint로 설정했다.)
    return은 상위의 긴급 탈출 명령어로 break보다도 유용히 사용된다.
    함수를 만들땐 main에 실제 값을 입력한다면 그 위에는 일종의 프로토 타입으로 미리 어떻게 형식을 취할거다 라고 예고해 주는 것이다.
    
      int calc(int a, int b, char op) 함수도 만들어 그 안에 명령어를 넣고 이것을 변수 하나를 만들어 실행 시켜준다.(result)
    실행된 변수를 printf에다 옮겨주면 이제 일련의 과정을 통해 계산의 결과가 나타나는 함수 계산기가 완성된다.

---
+++ calc1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+    int inputint() {
+        int x;
+    while (1) {
+      int result = scanf("%d", &x);
+        if (result != 1) {
+            printf("친구, 알지? 숫자. \n");
+            while (getchar() != '\n');
+            continue;
+        }
+return x;
+}
+}
+  int calc(int a, int b, char op) {
+switch (op) {
+    case '+' : return a + b;
+    case '-' : return a - b;
+    case '*' : return a * b;
+    case '/' :
+    if (b == 0) {
+        printf("0으로 못 나눠!\n");
+        return 0;
+    }
+    return a/b;
+    default:
+    printf("연산자 제대로 입력해야제..\n");
+    return 0;
+
+}
+
+    }
+int main() {
+    int a,b;
+    char op;
+    printf("첫 번째 정수: ");
+    a = inputint();
+    printf("두 번째 정수: ");
+    b = inputint();
+    printf("연산 선택 (+,-,*,/): ");
+    scanf(" %c", &op);
+    int result = calc(a, b, op);
+    printf("결과: %d\n", result);
+    return 0;
+}
